5f75be1b1a2da8734fc7a41f586832bd50177e8c
wumfi <peter@wumfi.com>
2023-05-20 23:07:10 +0100

    Auto commit 2023-05-20 23:07:09

--- sunset.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo date('H',date_sunrise(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1)).PHP_EOL;
-echo date('H',date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1)).PHP_EOL;
-?>
