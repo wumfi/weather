11faa338ab6d2e7a3577dceabd969aa37c732de6
wumfi <peter@wumfi.com>
2023-08-07 17:07:33 +0100

    Auto commit 2023-08-07 17:07:33

--- get_cond.php
+++ get_cond.php
@@ -24,7 +24,6 @@
 // 	2 = Will use predefined json to save API calls
 // 	3 = Demo reel
 $debug=1;
-$debugfileon=1;
 
 if($debug == 3) {
 	echo 9;
@@ -40,105 +39,44 @@ if(date('H')>22 or date('H')<7) {
 }
 
 // Define our current condition file
-$cond_file='/var/www/html/weather/current.cond';
-$debugfile='/var/www/html/weather/debug.txt';
-
-
-// If condition file does not exist, create it with dummy value
-if(!is_file($cond_file)){
-	$contents = 0;
-	file_put_contents($cond_file, $contents);
-}
-
-// Load my API key and define curl URL
-$apiKey=str_replace(array("\r\n", "\n", "\r"), '', file_get_contents('/var/www/html/weather/api_key'));
-$url='http://api.openweathermap.org/data/2.5/weather?id=2654308&appid='.$apiKey.'&units=metric';
-
-if($debug==1) {
-	// Initiate cURL
-	$ch=curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	// Grab response
-	$json=curl_exec($ch);
-} else {
-	$json='{"coord":{"lon":-0.13,"lat":50.96},"weather":[{"id":201,"main":"Clouds","description":"few clouds","icon":"02d"}],"base":"stations","main":{"temp":17.16,"pressure":1019,"humidity":77,"temp_min":15,"temp_max":18.89},"visibility":10000,"wind":{"speed":5.1,"deg":210},"clouds":{"all":20},"dt":1560761506,"sys":{"type":1,"id":1395,"message":0.0078,"country":"GB","sunrise":1560743143,"sunset":1560802623},"timezone":3600,"id":2654308,"name":"Burgess Hill","cod":200}';
-}
-
-// Convert response to an array
-$ary = json_decode($json, true);
-
-// Grab the current weather ID, or set demo reel
-if($debug==3) {
-	$cond_code=9;
-} else {
-	$cond_code=$ary['weather'][0]['id'];
-}
-
+$cond_file='/var/www/html/weather/api.cond';
+$cond_code=file_get_contents($cond_file);
 // Switch on $cond_code to set code defined by openweathermap.org
 switch ($cond_code) {
 	case ($cond_code >= 200 and $cond_code < 300):
-		$condition = 1; // Thunderstorm
+		echo 1; // Thunderstorm
 		break;
 	case ($cond_code >= 300 and $cond_code < 500):
-		$condition = 2; // Drizzle
+		echo 2; // Drizzle
 		break;
 	case ($cond_code >= 500 and $cond_code < 600):
-		$condition = 3; // Rain
+		echo 3; // Rain
 		break;
 	case ($cond_code >= 600 and $cond_code < 700):
-		$condition = 4; // Snow
+		echo 4; // Snow
 		break;
 	case ($cond_code >= 700 and $cond_code < 800):
-		$condition = 5; // Misty/fog
+		echo 5; // Misty/fog
 		break;
 	case ($cond_code == 800):
 		$sunrise = date_sunrise(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1);
 		$sunset = date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1);
 		if(time()>=$sunrise and time()<=$sunset) {
-			$condition = 6; // Clear day
+			echo 6; // Clear day
 		} else {
-			$condition = 7; // Clear night
+			echo 7; // Clear night
 		}
 		break;
 	case ($cond_code > 800):
 		$sunrise = date_sunrise(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1);
 		$sunset = date_sunset(time(), SUNFUNCS_RET_TIMESTAMP, 38.4, -9, 90, 1);
 		if(time()>=$sunrise and time()<=$sunset) {
-			$condition = $cond_code; // Cloudy
+			echo $cond_code; // Cloudy
 		} else {
-			$condition = 7; // Clear night
+			echo 7; // Clear night
 		}
 		break;
-		break;
 	case ($cond_code == 9):
-		$condition = 9; // Demo reel
+		echo 9; // Demo reel
 		break;
 }
-
-// Get the last value from our conditions file
-$last=file_get_contents($cond_file);
-// Only output condition if it's changed from last read
-if ($last != $condition) {
-
-	// Put condition code to stdout
-	echo $condition;
-	$debugtext="Result sent: {$condition}".PHP_EOL;
-	if($debugfileon==1) {
-		file_put_contents($debugfile,$debugtext,FILE_APPEND);
-	}
-
-	// Save the condition code to our conditions file
-	file_put_contents($cond_file,$condition);
-} else {
-
-	// Nothing has changed, therefore output previous result
-	echo $last;
-	$debugtext="Result sent: {$last}".PHP_EOL;
-	if($debugfileon==1) {
-		file_put_contents($debugfile,$debugtext,FILE_APPEND);
-	}
-}
-?>
