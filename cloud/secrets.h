42f6d9e2a2d4e77bf9b871ee4de551fc2606d6b1
Pete <pete@wumfi.com>
2025-01-01 21:27:36 +0000

    Auto commit 2025-01-01 21:27:34

--- cloud/secrets.h
+++ cloud/secrets.h
@@ -3,4 +3,5 @@
 #define OTA_PASS          "PeteC72&"
 
 const char* host = "home.wumfi.com";
-const char* fingerprint = "22 C4 C5 AC 7E 27 98 9C E2 B6 CD 12 85 72 BA 64 B2 82 D9 67";
+//const char* fingerprint = "22 C4 C5 AC 7E 27 98 9C E2 B6 CD 12 85 72 BA 64 B2 82 D9 67";
+const char* fingerprint = "DB 1C BD 87 1D B4 7D 81 91 DC A4 88 5D CD 36 16 39 41 9E 79";
