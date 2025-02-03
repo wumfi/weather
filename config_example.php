f21b1bb0cd4e9694433ec771034ef106b6e3cd9a
Pete <pete@wumfi.com>
2025-02-03 13:13:05 +0000

    Auto commit 2025-02-03 13:13:03

---
+++ config_example.php
@@ -0,0 +1,10 @@
+<?php
+// Basic connection settings
+$databaseHost = '';
+$databaseUsername = '';
+$databasePassword = '';
+$databaseName = '';
+
+// Connect to the database
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
