7b2edb14065c676b006fff9aaeabed2ca97fe95c
Pete <pete@wumfi.com>
2025-02-03 13:10:18 +0000

    Auto commit 2025-02-03 13:10:17

--- get_weather.sh
+++ get_weather.sh
@@ -1,2 +1,3 @@
-output=$(curl -s "https://api.openweathermap.org/data/2.5/weather?lat=50.967779&lon=-.114799&appid=f8d0bdd6dfb78b81f9bc0baf247c0002" | jq .weather[].id)
+api_key=$(cat api_key)
+output=$(curl -s "https://api.openweathermap.org/data/2.5/weather?lat=50.967779&lon=-.114799&appid=${api_key}" | jq .weather[].id)
 mysql -BN -hplexnas.wumfi.com -Dsmart_control -uwumfi -pBaileyDog -e "update weather set code=${output};"
